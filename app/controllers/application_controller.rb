c5a55aea134e08fe28180dd73a361038ef1e8bcc
Sebastian Herrera <jusherrerama@unal.edu.co>
2017-03-21 20:55:41 -0500

    inicio

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::API
+
   #include ActionController::MimeResponds
   #include ActionController::ImplicitRender
 
   respond_to :json
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
  
   #resources :users
   
@@ -6,5 +7,6 @@ Rails.application.routes.draw do
   resources :users, except: [:new, :create], :defaults => { :format => 'json' }
   #resources :users
   root  'home#index', :defaults => { :format => 'json' }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
